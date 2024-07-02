33c950c11534e43db4cb5210a2ed265332be4e93
Oleksii Dmytriiev <sysqwerty1984@gmail.com>
2024-07-02 21:09:43 +0300

    add API Services

---
+++ src/js/index.js
@@ -0,0 +1,33 @@
+import { queryParamsFilters, dataOptions, refs } from '../js/constants';
+import { serviceGetFilters } from '../js/services';
+import { renderPagination } from '../js/renderPagination';
+import { renderCategories } from '../js/renderCategories';
+
+window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+  if (e.matches) {
+    queryParamsFilters.set('limit', 12);
+  } else {
+    queryParamsFilters.set('limit', 9);
+  }
+  console.log(queryParamsFilters.toString());
+  serviceGetFilters();
+});
+
+serviceGetFilters()
+  .then(data => {
+    dataOptions.totalPages = data.totalPages;
+    renderCategories(data.results);
+    renderPagination(data.totalPages);
+
+    const activePage = Array.from(refs.pagination.childNodes).find(
+      p => p.dataset.id == data.page
+    );
+    console.log(activePage); // TODO remove
+    activePage.classList.add('active');
+  })
+  .catch(error => {
+    console.log(error); // TODO toast error message?
+  });
+
+refs.filter.addEventListener('click', onFilterClick);
+function onFilterClick(e) {}
---
+++ src/js/services.js
@@ -0,0 +1,127 @@
+import { BASE_URL, queryParamsFilters } from '../js/constants';
+
+const requestOptionsGET = {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+};
+
+// Exercises
+
+const queryParamsExercises = new URLSearchParams({
+  page: 1,
+  limit: window.matchMedia('(min-width: 768px)').matches ? 10 : 8,
+});
+
+export async function serviceGetExercises({
+  bodypart,
+  muscles,
+  equipment,
+  keyword,
+}) {
+  if (bodypart) queryParamsExercises.set('bodypart', bodypart);
+  if (muscles) queryParamsExercises.set('muscles', muscles);
+  if (equipment) queryParamsExercises.set('equipment', equipment);
+  if (keyword) queryParamsExercises.set('keyword', keyword);
+
+  const res = await fetch(
+    `${BASE_URL}exercises?${queryParamsExercises}`,
+    requestOptionsGET
+  );
+  if (!res.ok) {
+    throw new Error(`Ooops, status code: ${res.status}`);
+  }
+  return await res.json();
+}
+
+// TODO test
+// serviceGetExercises({ equipment: 'band' });
+
+// NOTE: in case the mail has already did a review it can not be reused
+// you will get error message in response in format: {message: "Such email already exists"}
+// it should be handled in the catch() with toast message
+export async function servicePatchExercisesByIdRating(
+  id,
+  { rate, email, review }
+) {
+  const requestOptionsPatch = {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ rate, email, review }),
+  };
+
+  const res = await fetch(
+    `${BASE_URL}exercises/${id}/rating`,
+    requestOptionsPatch
+  );
+  if (!res.ok) {
+    throw new Error(`Ooops, status code: ${res.status}`);
+  }
+  return await res.json();
+}
+
+// TODO test
+// servicePatchRating('64f389465ae26083f39b1a70', {
+//   rate: 5,
+//   email: 'test0000002@gmail.com',
+//   review: 'My best exercise',
+// });
+
+export async function serviceGetExercisesById(id) {
+  const res = await fetch(`${BASE_URL}exercises/${id}`, requestOptionsGET);
+  if (!res.ok) {
+    throw new Error(`Ooops, status code: ${res.status}`);
+  }
+  return await res.json();
+}
+
+// TODO test
+// serviceGetExercisesById('64f389465ae26083f39b1a70');
+
+// Filters
+
+export async function serviceGetFilters() {
+  const res = await fetch(
+    `${BASE_URL}filters?${queryParamsFilters}`,
+    requestOptionsGET
+  );
+  if (!res.ok) {
+    throw new Error(`Ooops, status code: ${res.status}`);
+  }
+  return await res.json();
+}
+
+// Quotes
+export async function serviceGetQuote() {
+  const res = await fetch(`${BASE_URL}quote`, requestOptionsGET);
+  if (!res.ok) {
+    throw new Error(`Ooops, status code: ${res.status}`);
+  }
+  return await res.json();
+}
+
+// TODO test
+// serviceGetQuote();
+
+// Subscriptions
+export async function servicePostSubscription(email) {
+  const requestOptionsPost = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email }),
+  };
+
+  const res = await fetch(`${BASE_URL}subscription`, requestOptionsPost);
+  if (!res.ok) {
+    throw new Error(`Ooops, status code: ${res.status}`);
+  }
+  return await res.json();
+}
+
+// TODO test
+// servicePostSubscription('test0000002@gmail.com');
