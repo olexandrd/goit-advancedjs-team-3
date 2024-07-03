2b2886f2e9d4390298c0fd8ebb3c4a0cbc726e5e
olexandrd <olexandrd@users.noreply.github.com>
2024-07-03 19:37:14 +0000

    Deploying to gh-pages from @ olexandrd/goit-advancedjs-team-3@5b34f46c797bbe587c918c46f600720d376812fd 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,14 +1,14 @@
-import"./assets/main-5eb84389.js";import"./assets/search-input-81799c6d.js";import"./assets/header-3d6609c1.js";import"./assets/vendor-9ceb8365.js";const f="https://your-energy.b.goit.study/api/",s={list:document.querySelector(".js-filtered-list"),filter:document.querySelector(".js-filters"),pagination:document.querySelector(".js-pagination")},r={MUSCLES:"Muscles",BODY_PARTS:"Body parts",EQUIPMENT:"Equipment"},a=new URLSearchParams({filter:r.MUSCLES,page:1,limit:window.matchMedia("(min-width: 768px)").matches?12:9}),o=new URLSearchParams({page:1,limit:window.matchMedia("(min-width: 768px)").matches?10:8}),p={method:"GET",headers:{"Content-Type":"application/json"}};async function g(){const t=await fetch(`${f}filters?${a}`,p);if(!t.ok)throw new Error(`Ooops, status code: ${t.status}`);return await t.json()}function h(t){const i=t.map(({filter:e,name:n,imgURL:l})=>`<li class="filtered-item">
-          <a href="#" class="filtered-link" data-filter="${e}">
+import"./assets/main-d788657c.js";import{r as s,s as g,q as a,F as d,a as n}from"./assets/quote-ab1b9322.js";import"./assets/search-input-81799c6d.js";import"./assets/vendor-9ceb8365.js";function m(e){const i=e.map(({filter:t,name:r,imgURL:c})=>`<li class="filtered-item">
+          <a href="#" class="filtered-link" data-filter="${t}">
             <img
-              src="${l}"
-              alt="${n}"
+              src="${c}"
+              alt="${r}"
               class="filter-img"
               width="80"
               height="80"
             />
-            <h3 class="filtered-title">${e}</h3>
-            <p class="filtered-text">${n}</p>
+            <h3 class="filtered-title">${t}</h3>
+            <p class="filtered-text">${r}</p>
           </a>
-        </li>`).join("");s.list.innerHTML=i}function u(t){let i="";for(let e=1;e<=t;e++)i+=`<li class="pagination-item" data-id="${e}"><a href="#" class="pagination-link">${e}</a></li>`;s.pagination.innerHTML=i}function c(){g().then(t=>{t.totalPages,h(t.results),u(t.totalPages),Array.from(s.pagination.childNodes).forEach(e=>{e.classList.remove("active")});const i=Array.from(s.pagination.childNodes).find(e=>e.dataset.id==t.page);i&&i.classList.add("active")}).catch(t=>{console.log(t)})}function w(t){const e=t.target.closest("li.pagination-item");if(!e)return;const n=Number(e.dataset.id);a.set("page",n),c()}function y(t){const e=t.target.closest("li[data-filter]");if(!e)return;document.querySelectorAll(".js-filters .active").forEach(d=>d.classList.remove("active")),e.classList.add("active");const l=e.dataset.filter,m=Object.keys(r).find(d=>r[d]===l);m&&(a.set("filter",r[m]),a.set("page",1),o.set("page",1),c())}window.matchMedia("(min-width: 768px)").addEventListener("change",t=>{t.matches?(a.set("limit",12),o.set("limit",10)):(a.set("limit",9),o.set("limit",8)),a.set("page",1),o.set("page",1),c()});c();s.filter.addEventListener("click",y);s.pagination.addEventListener("click",w);
+        </li>`).join("");s.list.innerHTML=i}function p(e){let i="";for(let t=1;t<=e;t++)i+=`<li class="pagination-item" data-id="${t}"><a href="#" class="pagination-link">${t}</a></li>`;s.pagination.innerHTML=i}function l(){g().then(e=>{e.totalPages,m(e.results),p(e.totalPages),Array.from(s.pagination.childNodes).forEach(t=>{t.classList.remove("active")});const i=Array.from(s.pagination.childNodes).find(t=>t.dataset.id==e.page);i&&i.classList.add("active")}).catch(e=>{console.log(e)})}function h(e){const t=e.target.closest("li.pagination-item");if(!t)return;const r=Number(t.dataset.id);a.set("page",r),l()}function u(e){const t=e.target.closest("li[data-filter]");if(!t)return;document.querySelectorAll(".js-filters .active").forEach(o=>o.classList.remove("active")),t.classList.add("active");const c=t.dataset.filter,f=Object.keys(d).find(o=>d[o]===c);f&&(a.set("filter",d[f]),a.set("page",1),n.set("page",1),l())}window.matchMedia("(min-width: 768px)").addEventListener("change",e=>{e.matches?(a.set("limit",12),n.set("limit",10)):(a.set("limit",9),n.set("limit",8)),a.set("page",1),n.set("page",1),l()});l();s.filter.addEventListener("click",u);s.pagination.addEventListener("click",h);
 //# sourceMappingURL=commonHelpers.js.map
