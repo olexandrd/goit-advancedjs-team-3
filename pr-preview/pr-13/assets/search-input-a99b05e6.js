d9002d354f3c07e7be124b3f5bc8c4f54e1ac084
Sysqwerty <Sysqwerty@users.noreply.github.com>
2024-07-03 12:39:46 +0000

    Deploy preview for PR 13 🛫

---
+++ pr-preview/pr-13/assets/search-input-a99b05e6.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".search-form"),c=document.querySelector(".search-input-icon.cross-icon");c.addEventListener("click",()=>{e.reset()});
+//# sourceMappingURL=search-input-a99b05e6.js.map
