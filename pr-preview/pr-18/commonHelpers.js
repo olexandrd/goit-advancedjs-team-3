761faaae6a171a7d69801df935cb880ebd8a4515
olexandrd <olexandrd@users.noreply.github.com>
2024-07-04 08:01:07 +0000

    Deploy preview for PR 18 🛫

---
+++ pr-preview/pr-18/commonHelpers.js
@@ -0,0 +1,31 @@
+import"./assets/main-f052b72e.js";import{r as a,s as u,a as x,q as n,b as r,F as g}from"./assets/quote-be463568.js";import{i as f}from"./assets/vendor-790bbfe5.js";import"./assets/search-input-81799c6d.js";function h(t){const s=t.map(({filter:e,name:i,imgURL:c})=>`<li class="filtered-item" data-filter="${e}" data-name="${i}">
+          <a href="#" class="filtered-link" >
+            <img
+              src="${c}"
+              alt="${i}"
+              class="filter-img"
+              width="80"
+              height="80"
+            />
+            <h3 class="filtered-title">${e}</h3>
+            <p class="filtered-text">${i}</p>
+          </a>
+        </li>`).join("");a.list.innerHTML=s}function m(t){let s="";for(let e=1;e<=t;e++)s+=`<li class="pagination-item" data-id="${e}"><a href="#" class="pagination-link">${e}</a></li>`;a.pagination.innerHTML=s}function v(t){const s=t.map(({name:e,rating:i,bodyPart:c,burnedCalories:l,target:o})=>`<li class="exercise-card" >
+          <h3 class="exercise-general-header">Workout</h3>
+          <p class="exercise-rating">${i} 
+            <svg>
+              <use href="./images/icons.svg#icon-star"></use>
+            </svg>
+          </p> 
+          <button class="exercise-start-btn" type="button">
+            start
+            <svg>
+              <use href="./images/icons.svg#icon-start-arrow"></use>
+            </svg>
+          </button>
+          <h2 class="exercise-name">${e}</h2>
+          <p class="exercise-text">Burned calories: ${l}</p>
+          <p class="exercise-text">Body part: ${c}</p>
+          <p class="exercise-text">Target: ${o}</p>
+        </li>`).join("");a.list.innerHTML=s}function d(){u().then(t=>{t.totalPages,h(t.results),m(t.totalPages),Array.from(a.pagination.childNodes).forEach(e=>{e.classList.remove("active")});const s=Array.from(a.pagination.childNodes).find(e=>e.dataset.id==t.page);s&&s.classList.add("active")}).catch(t=>{f.error({message:`${t}`})})}function p(t){x().then(s=>{v(s.results),m(s.totalPages),Array.from(a.pagination.childNodes).forEach(i=>{i.classList.remove("active")});const e=Array.from(a.pagination.childNodes).find(i=>i.dataset.id==s.page);e&&e.classList.add("active")}).catch(s=>{f.error({message:`${s}`})})}function $(t){const e=t.target.closest("li.pagination-item");if(!e)return;const i=Number(e.dataset.id);n.set("page",i),r.set("page",i),a.exerciseHeader.dataset.exercise?p():d()}function E(t){const e=t.target.closest("li[data-filter]");if(!e)return;document.querySelectorAll(".js-filters .active").forEach(o=>o.classList.remove("active")),e.classList.add("active");const c=e.dataset.filter,l=Object.keys(g).find(o=>g[o]===c);l&&(n.set("filter",g[l]),n.set("page",1),r.delete("muscles"),r.delete("bodypart"),r.delete("equipment"),r.set("page",1),d()),k()}function L(t){const e=t.target.closest("li.filtered-item");if(!e)return;let i=e.dataset.filter.toString().toLowerCase();i==="body parts"&&(i="bodypart");const c=e.dataset.name;y(c),r.set(i,c),r.set("page",1),p()}function y(t){a.exerciseHeader.textContent=` / ${t}`,a.exerciseHeader.dataset.exercise=t}function k(){a.exerciseHeader.textContent="",a.exerciseHeader.dataset.exercise=""}window.matchMedia("(min-width: 768px)").addEventListener("change",t=>{t.matches?(n.set("limit",12),r.set("limit",10)):(n.set("limit",9),r.set("limit",8)),n.set("page",1),r.set("page",1),d()});d();a.filter.addEventListener("click",E);a.pagination.addEventListener("click",$);a.list.addEventListener("click",L);
+//# sourceMappingURL=commonHelpers.js.map
