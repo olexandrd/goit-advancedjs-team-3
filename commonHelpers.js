e5ceaf2f67c7493e968a8dc797a6a46d6162485f
olexandrd <olexandrd@users.noreply.github.com>
2024-07-04 12:42:04 +0000

    Deploying to gh-pages from @ olexandrd/goit-advancedjs-team-3@2ad7d1fb83bd2e8e5c3ea74a7238ba19d374156a 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,29 +1,49 @@
-import"./assets/main-288ba5d6.js";import{r as a,s as u,a as h,q as n,b as r,F as g,c as v,d as x}from"./assets/quote-550f796a.js";import{i as f}from"./assets/vendor-790bbfe5.js";import"./assets/search-input-81799c6d.js";function E(t){const s=t.map(({filter:e,name:i,imgURL:c})=>`<li class="filtered-item" data-filter="${e}" data-name="${i}">
+import"./assets/main-8c51de94.js";import{r as s,s as u,a as h,q as n,b as r,F as f,c as L,d as $}from"./assets/quote-165641b1.js";import{i as g}from"./assets/vendor-790bbfe5.js";import"./assets/search-input-81799c6d.js";function E(e){const i=e.map(({filter:t,name:a,imgURL:c})=>`<li class="filtered-item" data-filter="${t}" data-name="${a}">
             <img
               src="${c}"
-              alt="${i}"
+              alt="${a}"
               class="filter-img"
               width="80"
               height="80"
             />
-            <h3 class="filtered-title">${e}</h3>
-            <p class="filtered-text">${i}</p>
-        </li>`).join("");a.list.innerHTML=s}function m(t){let s="";for(let e=1;e<=t;e++)s+=`<li class="pagination-item" data-id="${e}"><a href="#" onclick="event.preventDefault();" class="pagination-link">${e}</a></li>`;a.pagination.innerHTML=s}function L(t){const s=t.map(({name:e,rating:i,bodyPart:c,burnedCalories:l,target:o})=>`<li class="exercise-card" >
-          <h3 class="exercise-general-header">Workout</h3>
-          <p class="exercise-rating">${i} 
-            <svg>
-              <use href="./images/icons.svg#icon-star"></use>
-            </svg>
-          </p> 
-          <button class="exercise-start-btn" type="button">
-            start
-            <svg>
-              <use href="./images/icons.svg#icon-start-arrow"></use>
-            </svg>
-          </button>
-          <h2 class="exercise-name">${e}</h2>
-          <p class="exercise-text">Burned calories: ${l}</p>
-          <p class="exercise-text">Body part: ${c}</p>
-          <p class="exercise-text">Target: ${o}</p>
-        </li>`).join("");a.list.innerHTML=s}function d(){u().then(t=>{t.totalPages,E(t.results),m(t.totalPages),Array.from(a.pagination.childNodes).forEach(e=>{e.classList.remove("active")});const s=Array.from(a.pagination.childNodes).find(e=>e.dataset.id==t.page);s&&s.classList.add("active")}).catch(t=>{f.error({message:`${t}`})})}function p(){h().then(t=>{L(t.results),m(t.totalPages),Array.from(a.pagination.childNodes).forEach(e=>{e.classList.remove("active")});const s=Array.from(a.pagination.childNodes).find(e=>e.dataset.id==t.page);s&&s.classList.add("active")}).catch(t=>{f.error({message:`${t}`})})}function $(t){const e=t.target.closest("li.pagination-item");if(!e)return;const i=Number(e.dataset.id);n.set("page",i),r.set("page",i),a.exerciseHeader.dataset.exercise?p():d()}function y(t){const e=t.target.closest("li[data-filter]");if(!e)return;document.querySelectorAll(".js-filters .active").forEach(o=>o.classList.remove("active")),e.classList.add("active");const c=e.dataset.filter,l=Object.keys(g).find(o=>g[o]===c);l&&(n.set("filter",g[l]),n.set("page",1),r.delete("muscles"),r.delete("bodypart"),r.delete("equipment"),r.set("page",1),d()),v()}function k(t){const e=t.target.closest("li.filtered-item");if(!e)return;let i=e.dataset.filter.toString().toLowerCase();i==="body parts"&&(i="bodypart");const c=e.dataset.name;x(c),r.set(i,c),r.set("page",1),p()}window.matchMedia("(min-width: 768px)").addEventListener("change",t=>{t.matches?(n.set("limit",12),r.set("limit",10)):(n.set("limit",9),r.set("limit",8)),n.set("page",1),r.set("page",1),d()});d();a.filter.addEventListener("click",y);a.pagination.addEventListener("click",$);a.list.addEventListener("click",k);
+            <h3 class="filtered-title">${t}</h3>
+            <p class="filtered-text">${a}</p>
+        </li>`).join("");s.list.innerHTML=i}function v(e){let i="";for(let t=1;t<=e;t++)i+=`<li class="pagination-item" data-id="${t}"><a href="#" onclick="event.preventDefault();" class="pagination-link">${t}</a></li>`;s.pagination.innerHTML=i}const y=e=>e&&e.charAt(0).toUpperCase()+e.slice(1);function k(e){const i=e.map(({_id:t,name:a,rating:c,bodyPart:o,burnedCalories:l,target:p,time:x})=>`<li class="exercise-card">
+          <div class="exercise-card-top">
+            <div class="exercise-card-top-info">
+              <h3 class="exercise-general-header">Workout</h3>
+              <p class="exercise-rating">${c}<svg class="exercise-rating-icon">
+                  <use href="./images/icons.svg#icon-star"></use>
+                </svg>
+              </p> 
+            </div>
+            <button class="exercise-start-btn" data-exercise-id="${t}" type="button">
+              Start<svg class="exercise-start-icon">
+                <use href="./images/icons.svg#icon-start-arrow"></use>
+              </svg>
+            </button>
+          </div>
+          <div class="exercise-card-bottom">
+            <div class="exercise-card-title">
+              <svg class="exercise-card-icon">
+                <use href="./images/icons.svg#icon-run"></use>
+              </svg>
+              <p class="exercise-card-title-text">${y(a)}</p>
+            </div>
+            <div class="exercise-card-info"> 
+              <div class="exercise-card-info-element">
+                <div class="exercise-card-info-element-heading">Burned calories:</div>
+                <div class="exercise-card-info-element-content-target-no-overflow">${l} / ${x} min</div>
+              </div>
+              <div class="exercise-card-info-element">
+                <div class="exercise-card-info-element-heading">Body part:</div>
+                <div class="exercise-card-info-element-content-target-no-overflow">${o}</div>
+              </div>
+              <div class="exercise-card-info-element">
+                <div class="exercise-card-info-element-heading">Target:</div>
+                <div class="exercise-card-info-element-content-target-no-overflow">${p}</div>
+              </div>
+            </div>
+          </div>
+        </li>`).join("");s.list.innerHTML=i}function d(){u().then(e=>{e.totalPages,E(e.results),v(e.totalPages),Array.from(s.pagination.childNodes).forEach(t=>{t.classList.remove("active")});const i=Array.from(s.pagination.childNodes).find(t=>t.dataset.id==e.page);i&&i.classList.add("active")}).catch(e=>{g.error({message:`${e}`})})}function m(){h().then(e=>{k(e.results),v(e.totalPages),Array.from(s.pagination.childNodes).forEach(t=>{t.classList.remove("active")});const i=Array.from(s.pagination.childNodes).find(t=>t.dataset.id==e.page);i&&i.classList.add("active")}).catch(e=>{g.error({message:`${e}`})})}function b(e){const t=e.target.closest("li.pagination-item");if(!t)return;const a=Number(t.dataset.id);n.set("page",a),r.set("page",a),s.exerciseHeaderSpan.dataset.exercise?m():d()}function w(e){const t=e.target.closest("li[data-filter]");if(!t)return;document.querySelectorAll(".js-filters .active").forEach(l=>l.classList.remove("active")),t.classList.add("active");const c=t.dataset.filter,o=Object.keys(f).find(l=>f[l]===c);o&&(n.set("filter",f[o]),n.set("page",1),r.delete("muscles"),r.delete("bodypart"),r.delete("equipment"),r.set("page",1),s.list.classList.replace("exercise-list","filtered-list"),d()),L()}function P(e){const t=e.target.closest("li.filtered-item");if(!t)return;let a=t.dataset.filter.toString().toLowerCase();a==="body parts"&&(a="bodypart");const c=t.dataset.name;$(c),r.set(a,c),r.set("page",1),s.list.classList.replace("filtered-list","exercise-list"),s.list.innerHTML="",m()}window.matchMedia("(min-width: 768px)").addEventListener("change",e=>{e.matches?(n.set("limit",12),r.set("limit",10)):(n.set("limit",9),r.set("limit",8)),n.set("page",1),r.set("page",1),d()});d();s.filter.addEventListener("click",w);s.pagination.addEventListener("click",b);s.list.addEventListener("click",P);
 //# sourceMappingURL=commonHelpers.js.map
