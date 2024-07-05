befbad533bac09465aa0db22c6a85940240cf483
LadaM <LadaM@users.noreply.github.com>
2024-07-05 19:49:36 +0000

    Deploy preview for PR 53 🛫

--- pr-preview/pr-53/assets/search-input-1bf2cfc1.js
+++
@@ -1,2 +0,0 @@
-import{r as e,a as r,i as a,j as s}from"./eventHandlers-6755cbad.js";e.searchInputCrossIcon.addEventListener("click",()=>{r.set("page",1),r.delete("keyword"),e.searchForm.reset(),a()});e.searchInputSearchIcon.addEventListener("click",s);e.searchForm.addEventListener("submit",s);
-//# sourceMappingURL=search-input-1bf2cfc1.js.map
