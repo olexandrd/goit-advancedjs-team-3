b9173dd9a4a9d72a8e74f489aaebc92109fe2033
LadaM <LadaM@users.noreply.github.com>
2024-07-05 20:41:30 +0000

    Deploy preview for PR 53 🛫

---
+++ pr-preview/pr-53/assets/search-input-d9099c9f.js
@@ -0,0 +1,2 @@
+import{r as e,a as r,i as a,j as s}from"./eventHandlers-58a21607.js";e.searchInputCrossIcon.addEventListener("click",()=>{r.set("page",1),r.delete("keyword"),e.searchForm.reset(),a()});e.searchInputSearchIcon.addEventListener("click",s);e.searchForm.addEventListener("submit",s);
+//# sourceMappingURL=search-input-d9099c9f.js.map
