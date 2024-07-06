0363ff167b4aa27632b37d49d897a86599f3dd13
LadaM <lada11lada@gmail.com>
2024-07-06 12:06:29 +0000

    Deploying to gh-pages from @ olexandrd/goit-advancedjs-team-3@891d9bc1d637c1683a1c46cf85ffb052bcba1b9d 🚀

--- pr-preview/pr-68/assets/search-input-642cca9b.js
+++
@@ -1,2 +0,0 @@
-import{r as e,a as r,h as a,i as s}from"./eventHandlers-a59774f4.js";e.searchInputCrossIcon.addEventListener("click",()=>{r.set("page",1),r.delete("keyword"),e.searchForm.reset(),a()});e.searchInputSearchIcon.addEventListener("click",s);e.searchForm.addEventListener("submit",s);
-//# sourceMappingURL=search-input-642cca9b.js.map
