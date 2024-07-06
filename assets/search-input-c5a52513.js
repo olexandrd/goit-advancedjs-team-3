82d2c05978a0a2edcfde02b25db03c0ae4ad5ab2
olexandrd <olexandrd@users.noreply.github.com>
2024-07-06 08:55:22 +0000

    Deploying to gh-pages from @ olexandrd/goit-advancedjs-team-3@260d8b0ce7203fc56c4b0af35b224a8abb2b652d 🚀

--- assets/search-input-c5a52513.js
+++
@@ -1,2 +0,0 @@
-import{r as e,a as r,k as a,l as s}from"./eventHandlers-1647f2aa.js";e.searchInputCrossIcon.addEventListener("click",()=>{r.set("page",1),r.delete("keyword"),e.searchForm.reset(),a()});e.searchInputSearchIcon.addEventListener("click",s);e.searchForm.addEventListener("submit",s);
-//# sourceMappingURL=search-input-c5a52513.js.map
--- pr-preview/pr-54/assets/search-input-d1df183b.js
+++
@@ -1,2 +0,0 @@
-import{r as e,a as r,l as a,m as s}from"./eventHandlers-3c8eadc7.js";e.searchInputCrossIcon.addEventListener("click",()=>{r.set("page",1),r.delete("keyword"),e.searchForm.reset(),a()});e.searchInputSearchIcon.addEventListener("click",s);e.searchForm.addEventListener("submit",s);
-//# sourceMappingURL=search-input-d1df183b.js.map
