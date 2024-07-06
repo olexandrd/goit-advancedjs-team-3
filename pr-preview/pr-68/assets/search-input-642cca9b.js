75ad38c11b3d0bf48b9d074c3a997ad6073182ae
LadaM <LadaM@users.noreply.github.com>
2024-07-06 12:00:20 +0000

    Deploy preview for PR 68 🛫

---
+++ pr-preview/pr-68/assets/search-input-642cca9b.js
@@ -0,0 +1,2 @@
+import{r as e,a as r,h as a,i as s}from"./eventHandlers-a59774f4.js";e.searchInputCrossIcon.addEventListener("click",()=>{r.set("page",1),r.delete("keyword"),e.searchForm.reset(),a()});e.searchInputSearchIcon.addEventListener("click",s);e.searchForm.addEventListener("submit",s);
+//# sourceMappingURL=search-input-642cca9b.js.map
