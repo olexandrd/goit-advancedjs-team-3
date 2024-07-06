d0908ef8c81e50c6fc932a9db881c542279be1bb
Sysqwerty <Sysqwerty@users.noreply.github.com>
2024-07-06 08:17:41 +0000

    Deploy preview for PR 54 🛫

---
+++ pr-preview/pr-54/assets/search-input-f4064b64.js
@@ -0,0 +1,2 @@
+import{r as e,a as r,l as a,m as s}from"./eventHandlers-3ce4a55d.js";e.searchInputCrossIcon.addEventListener("click",()=>{r.set("page",1),r.delete("keyword"),e.searchForm.reset(),a()});e.searchInputSearchIcon.addEventListener("click",s);e.searchForm.addEventListener("submit",s);
+//# sourceMappingURL=search-input-f4064b64.js.map
