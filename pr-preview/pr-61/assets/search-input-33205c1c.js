e59bc39d7c0d5b92135ed724df9013aef68e4387
kazamov <kazamov@users.noreply.github.com>
2024-07-06 10:27:10 +0000

    Deploy preview for PR 61 🛫

---
+++ pr-preview/pr-61/assets/search-input-33205c1c.js
@@ -0,0 +1,2 @@
+import{r as e,a as r,m as a,n as s}from"./eventHandlers-59dba57a.js";e.searchInputCrossIcon.addEventListener("click",()=>{r.set("page",1),r.delete("keyword"),e.searchForm.reset(),a()});e.searchInputSearchIcon.addEventListener("click",s);e.searchForm.addEventListener("submit",s);
+//# sourceMappingURL=search-input-33205c1c.js.map
