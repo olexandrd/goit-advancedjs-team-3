c36f6604fe9dd46f1f924b608a2de9a1f89fb5a8
juliacheperis13 <35917877+juliacheperis13@users.noreply.github.com>
2024-07-06 18:15:01 +0000

    Deploying to gh-pages from @ olexandrd/goit-advancedjs-team-3@e64a560c4a815b528b1b00df54d10c958ae336fe 🚀

--- assets/search-input-31dd0972.js
+++
@@ -1,2 +0,0 @@
-import{r as e,a as r,h as a,i as s}from"./eventHandlers-97127148.js";e.searchInputCrossIcon.addEventListener("click",()=>{r.set("page",1),r.delete("keyword"),e.searchForm.reset(),a()});e.searchInputSearchIcon.addEventListener("click",s);e.searchForm.addEventListener("submit",s);
-//# sourceMappingURL=search-input-31dd0972.js.map
--- pr-preview/pr-76/assets/search-input-6867600b.js
+++
@@ -1,2 +0,0 @@
-import{r as e,a as r,h as a,i as s}from"./eventHandlers-5b4648f6.js";e.searchInputCrossIcon.addEventListener("click",()=>{r.set("page",1),r.delete("keyword"),e.searchForm.reset(),a()});e.searchInputSearchIcon.addEventListener("click",s);e.searchForm.addEventListener("submit",s);
-//# sourceMappingURL=search-input-6867600b.js.map
