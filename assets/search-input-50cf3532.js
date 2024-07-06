9291d05d71a3133a5b687f2d24bb350c1f6a0853
kazamov <kazamov@gmail.com>
2024-07-06 12:12:02 +0000

    Deploying to gh-pages from @ olexandrd/goit-advancedjs-team-3@33449dec19c738d8b84fd307cdd361ddb6257e64 🚀

---
+++ assets/search-input-50cf3532.js
@@ -0,0 +1,2 @@
+import{r as e,a as r,m as a,n as s}from"./eventHandlers-79910cfc.js";e.searchInputCrossIcon.addEventListener("click",()=>{r.set("page",1),r.delete("keyword"),e.searchForm.reset(),a()});e.searchInputSearchIcon.addEventListener("click",s);e.searchForm.addEventListener("submit",s);
+//# sourceMappingURL=search-input-50cf3532.js.map
