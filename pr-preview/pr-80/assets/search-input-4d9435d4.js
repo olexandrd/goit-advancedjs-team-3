71511c78674107b904f507a9da8570c9a3767081
LadaM <LadaM@users.noreply.github.com>
2024-07-06 21:49:29 +0000

    Deploy preview for PR 80 🛫

--- pr-preview/pr-80/assets/search-input-4d9435d4.js
+++
@@ -1,2 +0,0 @@
-import{r as e,a as r,h as a,i as s}from"./eventHandlers-042f6d40.js";e.searchInputCrossIcon.addEventListener("click",()=>{r.set("page",1),r.delete("keyword"),e.searchForm.reset(),a()});e.searchInputSearchIcon.addEventListener("click",s);e.searchForm.addEventListener("submit",s);
-//# sourceMappingURL=search-input-4d9435d4.js.map
