e7615304c6fe2ab4f8fd8fea2ed539ea571628d4
juliacheperis13 <juliacheperis13@users.noreply.github.com>
2024-07-06 17:56:18 +0000

    Deploy preview for PR 75 🛫

---
+++ pr-preview/pr-75/assets/search-input-e1d99a49.js
@@ -0,0 +1,2 @@
+import{r as e,a as r,h as a,i as s}from"./eventHandlers-6740ae84.js";e.searchInputCrossIcon.addEventListener("click",()=>{r.set("page",1),r.delete("keyword"),e.searchForm.reset(),a()});e.searchInputSearchIcon.addEventListener("click",s);e.searchForm.addEventListener("submit",s);
+//# sourceMappingURL=search-input-e1d99a49.js.map
