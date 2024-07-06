4918df83fb6452fdccc9b3b3915ff4006b236c2b
Sysqwerty <Sysqwerty@users.noreply.github.com>
2024-07-06 18:46:01 +0000

    Deploy preview for PR 77 🛫

---
+++ pr-preview/pr-77/assets/search-input-76f35304.js
@@ -0,0 +1,2 @@
+import{r as e,a as r,h as a,i as s}from"./eventHandlers-35f83ab6.js";e.searchInputCrossIcon.addEventListener("click",()=>{r.set("page",1),r.delete("keyword"),e.searchForm.reset(),a()});e.searchInputSearchIcon.addEventListener("click",s);e.searchForm.addEventListener("submit",s);
+//# sourceMappingURL=search-input-76f35304.js.map
