26d28284ccd5e7447ec4d161e59e794ff66bcf76
olexandrd <olexandrd@users.noreply.github.com>
2024-07-06 12:05:12 +0000

    Deploy preview for PR 67 🛫

--- pr-preview/pr-67/assets/search-input-511dc1d7.js
+++
@@ -1,2 +0,0 @@
-import{r as e,a as r,m as a,n as s}from"./eventHandlers-59ac7443.js";e.searchInputCrossIcon.addEventListener("click",()=>{r.set("page",1),r.delete("keyword"),e.searchForm.reset(),a()});e.searchInputSearchIcon.addEventListener("click",s);e.searchForm.addEventListener("submit",s);
-//# sourceMappingURL=search-input-511dc1d7.js.map
---
+++ pr-preview/pr-67/assets/search-input-81fa4fe9.js
@@ -0,0 +1,2 @@
+import{r as e,a as r,m as a,n as s}from"./eventHandlers-8a2cd493.js";e.searchInputCrossIcon.addEventListener("click",()=>{r.set("page",1),r.delete("keyword"),e.searchForm.reset(),a()});e.searchInputSearchIcon.addEventListener("click",s);e.searchForm.addEventListener("submit",s);
+//# sourceMappingURL=search-input-81fa4fe9.js.map
