240931b6308fc7b6a59ad2ea885b2098fc4cca6e
olexandrd <olexandrd@users.noreply.github.com>
2024-07-07 08:42:00 +0000

    Deploying to gh-pages from @ olexandrd/goit-advancedjs-team-3@352d00dd6cb1a95c46bc6c5a6baa22a97b0458c0 🚀

---
+++ assets/search-input-27f1985b.js
@@ -0,0 +1,2 @@
+import{r as e,a as r,h as a,i as s}from"./eventHandlers-0b413571.js";e.searchInputCrossIcon.addEventListener("click",()=>{r.set("page",1),r.delete("keyword"),e.searchForm.reset(),a()});e.searchInputSearchIcon.addEventListener("click",s);e.searchForm.addEventListener("submit",s);
+//# sourceMappingURL=search-input-27f1985b.js.map
