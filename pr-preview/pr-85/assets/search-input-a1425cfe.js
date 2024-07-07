04c39af3e6fb559dcfbe66cbe8914359ac094fa4
Sysqwerty <Sysqwerty@users.noreply.github.com>
2024-07-07 10:18:19 +0000

    Deploy preview for PR 85 🛫

--- pr-preview/pr-85/assets/search-input-a1425cfe.js
+++
@@ -1,2 +0,0 @@
-import{r as e,a as r,i as a,j as s}from"./eventHandlers-eb124e4e.js";e.searchInputCrossIcon.addEventListener("click",()=>{r.set("page",1),r.delete("keyword"),e.searchForm.reset(),a()});e.searchInputSearchIcon.addEventListener("click",s);e.searchForm.addEventListener("submit",s);
-//# sourceMappingURL=search-input-a1425cfe.js.map
