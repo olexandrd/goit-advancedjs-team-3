0a8fef6a2d4488e7fad650b34adf5611214114c9
Sysqwerty <sysqwerty1984@gmail.com>
2024-07-07 10:23:31 +0000

    Deploying to gh-pages from @ olexandrd/goit-advancedjs-team-3@e5a5db09a710e448d4e9c81b8929eac8a1cb4005 🚀

---
+++ assets/search-input-bc65f885.js
@@ -0,0 +1,2 @@
+import{r as e,a as r,i as a,j as s}from"./eventHandlers-1d7ab357.js";e.searchInputCrossIcon.addEventListener("click",()=>{r.set("page",1),r.delete("keyword"),e.searchForm.reset(),a()});e.searchInputSearchIcon.addEventListener("click",s);e.searchForm.addEventListener("submit",s);
+//# sourceMappingURL=search-input-bc65f885.js.map
--- pr-preview/pr-85/assets/search-input-27eda333.js
+++
@@ -1,2 +0,0 @@
-import{r as e,a as r,i as a,j as s}from"./eventHandlers-91cd78e9.js";e.searchInputCrossIcon.addEventListener("click",()=>{r.set("page",1),r.delete("keyword"),e.searchForm.reset(),a()});e.searchInputSearchIcon.addEventListener("click",s);e.searchForm.addEventListener("submit",s);
-//# sourceMappingURL=search-input-27eda333.js.map
