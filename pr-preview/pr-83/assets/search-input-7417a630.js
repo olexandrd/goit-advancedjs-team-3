19f41ff2e268d4bb64d7fea8990d8b79f32c7c39
olexandrd <olexandrd@users.noreply.github.com>
2024-07-07 08:02:54 +0000

    Deploy preview for PR 83 🛫

---
+++ pr-preview/pr-83/assets/search-input-7417a630.js
@@ -0,0 +1,2 @@
+import{r as e,a as r,h as a,i as s}from"./eventHandlers-37bc695f.js";e.searchInputCrossIcon.addEventListener("click",()=>{r.set("page",1),r.delete("keyword"),e.searchForm.reset(),a()});e.searchInputSearchIcon.addEventListener("click",s);e.searchForm.addEventListener("submit",s);
+//# sourceMappingURL=search-input-7417a630.js.map
