76c10ddbe5a3b8a0ee64aa89fed1bf50ce4e0204
Sysqwerty <Sysqwerty@users.noreply.github.com>
2024-07-07 17:57:37 +0000

    Deploy preview for PR 87 🛫

---
+++ pr-preview/pr-87/assets/ratingPopup-1f71edcf.js
@@ -0,0 +1,20 @@
+import{e as g,d as b,i as l}from"./vendor-347e8cf8.js";import{g as S,r as n,h as v,i as h}from"./eventHandlers-39254920.js";const a=document.querySelector(".js-menu-container"),c=document.querySelector(".js-open-menu"),q=document.querySelector(".js-close-menu"),w=document.querySelectorAll(".mobile-menu-link"),o=()=>{const e=c.getAttribute("aria-expanded")==="true"||!1;c.setAttribute("aria-expanded",!e),a.classList.toggle("is-open"),e?(g(document.body),document.removeEventListener("keydown",u),a.removeEventListener("click",m)):(b(document.body),document.addEventListener("keydown",u),a.addEventListener("click",m))},u=e=>{e.key==="Escape"&&o()},m=e=>{e.target===a&&o()};c.addEventListener("click",o);q.addEventListener("click",o);w.forEach(e=>{e.addEventListener("click",o)});window.matchMedia("(min-width: 768px)").addEventListener("change",e=>{e.matches&&(a.classList.remove("is-open"),c.setAttribute("aria-expanded",!1),g(document.body),document.removeEventListener("keydown",u),a.removeEventListener("click",m))});(async()=>{const e=document.getElementById("quote");if(!e)return;let t;const i=localStorage.getItem("quote");if(i&&(t=JSON.parse(i),Date.now()-t.timestamp>1e3*60*60*24&&(t=null)),!t)try{t=await S(),localStorage.setItem("quote",JSON.stringify({...t,timestamp:Date.now()}))}catch{t=null}if(!t)return;const r=e.querySelector(".quote-description"),E=e.querySelector(".quote-author");r.textContent=t.quote,E.textContent=t.author,e.classList.remove("hidden")})();n.scrollUpButton.addEventListener("click",()=>{window.scrollTo({top:0,behavior:"smooth"})});let s=0,p;function k(e){e.preventDefault(),s&&p?h(p,{rate:Number(s),email:n.ratingForm.elements.email.value,review:n.ratingForm.elements.comment.value}).then(()=>{d(),l.success({message:"Thanks for rating!"}),n.ratingForm.reset(),s=0}).catch(t=>{l.error({message:t.message})}):l.warning({message:"Please select rating"})}function f(e){const t=e.target.closest(".rating-star-container");if(t){const i=document.querySelectorAll(".js-rating-star");s=t.dataset.id,document.querySelector(".js-rating-label").textContent=Number(s).toFixed(1);for(let r=0;r<s;r++)i[r].classList.add("rating-star--active");for(let r=s;r<5;r++)i[r].classList.remove("rating-star--active")}}function B(){p=JSON.parse(document.querySelector("button[data-exercise]").dataset.exercise)._id,n.exercisePopUpBackdrop.classList.remove("is-open"),n.exercisePopUpBackdrop.style.display="none",n.ratingPopUpBackdrop.classList.add("is-open"),n.ratingPopUpContent.innerHTML=P(),document.removeEventListener("keydown",v),C()}function d(){n.ratingPopUpBackdrop.classList.remove("is-open"),n.exercisePopUpBackdrop.style.display="flex",n.exercisePopUpBackdrop.classList.add("is-open"),document.addEventListener("keydown",v),M()}function L(e){e.code==="Escape"&&d()}function x(){let e="";for(let t=0;t<5;t++)e+=`
+    <div class="rating-star-container" data-id="${t+1}">
+      <svg class="rating-star js-rating-star">
+        <use href="./images/icons.svg#icon-star"></use>
+      </svg>
+    </div>`;return`
+    <div class="rating rating--dark">
+      <span class="rating-label js-rating-label">0.0</span>
+        ${e}
+      </div>`}function P(){return`
+    <p class="rating-data-parameter-name">Rating</p>
+    <div class="rating-rating-wrapper">
+    <div class="dark-container">
+      <div class="sub-container">
+      ${x()}
+      </div>
+    </div>
+    </div>
+  `}function C(){n.ratingPopUpCloseBtn.addEventListener("click",d),n.ratingPopUpContent.addEventListener("click",f),n.ratingForm.addEventListener("submit",k),document.addEventListener("keydown",L)}function M(){n.ratingPopUpCloseBtn.removeEventListener("click",d),n.ratingPopUpContent.removeEventListener("click",f),n.ratingForm.removeEventListener("submit",k),document.removeEventListener("keydown",L)}function y(){const e=document.querySelector(".js-add-rating");return e?(e.addEventListener("click",B),!0):!1}const U=new MutationObserver(e=>{e.forEach(t=>{(t.type==="childList"||t.type==="subtree")&&y()})});U.observe(document,{childList:!0,subtree:!0});y();
+//# sourceMappingURL=ratingPopup-1f71edcf.js.map
